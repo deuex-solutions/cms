5a7ef19057ca76b7d6a206d7a2e5fffc2996e0e6
Sanket Shah <er.sanketshah@gmail.com>
2020-03-13 16:37:03 +0530

    Setting app with vue, vue-material, proxy

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>CMS Index</p>
+    <md-button class="md-primary">Submit</md-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index'
+}
+</script>
+
+<style>
+</style>
