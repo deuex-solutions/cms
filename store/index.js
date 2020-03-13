97ba1aadec6097a131b89bc68b262e60d95589d9
Sanket Shah <er.sanketshah@gmail.com>
2020-03-14 03:06:10 +0530

    Fixed vuex store warning message

--- store/index.js
+++ store/index.js
@@ -3,76 +3,71 @@ import md5 from 'md5'
 import db from '~/plugins/firestore'
 import { saveUserData, clearUserData } from '~/utils'
 
-const createStore = () => {
-  return new Vuex.Store({
-    state: {
-      loading: false,
-      token: '',
-      user: null
-    },
-    mutations: {
-      setLoading(state, loading) {
-        state.loading = loading
-      },
-      setUser(state, user) {
-        state.user = user
-      },
-      setToken(state, token) {
-        state.token = token
-      },
-      clearToken: state => (state.token = ''),
-      clearUser: state => (state.user = null)
-    },
-    actions: {
-      async authenticateUser({ commit }, userPayload) {
-        try {
-          commit('setLoading', true)
-          const authUserData = await this.$axios.$post(
-            `/${userPayload.action}/`,
-            {
-              email: userPayload.email,
-              password: userPayload.password,
-              returnSecureToken: userPayload.returnSecureToken
-            }
-          )
-          let user
-          if (userPayload.action === 'register') {
-            const avatar = `http://gravatar.com/avatar/${md5(
-              authUserData.email
-            )}?d=identicon`
-            user = { email: authUserData.email, avatar }
-            await db
-              .collection('users')
-              .doc(userPayload.email)
-              .set(user)
-          } else {
-            const loginRef = db.collection('users').doc(userPayload.email)
-            const loggedInUser = await loginRef.get()
-            user = loggedInUser.data()
-          }
-          commit('setUser', user)
-          commit('setToken', authUserData.idToken)
-          commit('setLoading', false)
-          saveUserData(authUserData, user)
-        } catch (err) {
-          commit('setLoading', false)
-          console.error(err)
-        }
-      },
-      setLogoutTimer({ dispatch }, interval) {
-        setTimeout(() => dispatch('logoutUser'), interval)
-      },
-      logoutUser({ commit }) {
-        commit('clearToken')
-        commit('clearUser')
-        clearUserData()
-      }
-    },
-    getters: {
-      loading: state => state.loading,
-      isAuthenticated: state => !!state.token,
-      user: state => state.user
-    }
-  })
+export const state = () => ({
+  loading: false,
+  token: '',
+  user: null
+})
+
+export const mutations = {
+  setLoading(state, loading) {
+    state.loading = loading
+  },
+  setUser(state, user) {
+    state.user = user
+  },
+  setToken(state, token) {
+    state.token = token
+  },
+  clearToken: state => (state.token = ''),
+  clearUser: state => (state.user = null)
 }
-export default createStore
+
+export const actions = {
+  async authenticateUser({ commit }, userPayload) {
+    try {
+      commit('setLoading', true)
+      const authUserData = await this.$axios.$post(`/${userPayload.action}/`, {
+        email: userPayload.email,
+        password: userPayload.password,
+        returnSecureToken: userPayload.returnSecureToken
+      })
+      let user
+      if (userPayload.action === 'register') {
+        const avatar = `http://gravatar.com/avatar/${md5(
+          authUserData.email
+        )}?d=identicon`
+        user = { email: authUserData.email, avatar }
+        await db
+          .collection('users')
+          .doc(userPayload.email)
+          .set(user)
+      } else {
+        const loginRef = db.collection('users').doc(userPayload.email)
+        const loggedInUser = await loginRef.get()
+        user = loggedInUser.data()
+      }
+      commit('setUser', user)
+      commit('setToken', authUserData.idToken)
+      commit('setLoading', false)
+      saveUserData(authUserData, user)
+    } catch (err) {
+      commit('setLoading', false)
+      console.error(err)
+    }
+  },
+  setLogoutTimer({ dispatch }, interval) {
+    setTimeout(() => dispatch('logoutUser'), interval)
+  },
+  logoutUser({ commit }) {
+    commit('clearToken')
+    commit('clearUser')
+    clearUserData()
+  }
+}
+
+export const getters = {
+  loading: state => state.loading,
+  isAuthenticated: state => !!state.token,
+  user: state => state.user
+}
